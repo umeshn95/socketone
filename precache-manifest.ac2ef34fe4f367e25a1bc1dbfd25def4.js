230daba1ac6bc4b2c90cb5b656f4fc7176d0313e
umesh_n95 <umeshnamdev9@gmail.com>
2021-07-05 08:09:57 +0100

    Updates

---
+++ precache-manifest.ac2ef34fe4f367e25a1bc1dbfd25def4.js
@@ -0,0 +1,98 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f5a9eed54ee597a0c257f5aac821467f",
+    "url": "//umeshn95.github.io/index.html"
+  },
+  {
+    "revision": "bbfc2cf6a9202f3dd7bf",
+    "url": "//umeshn95.github.io/static/css/2.4707e12a.chunk.css"
+  },
+  {
+    "revision": "bbfc2cf6a9202f3dd7bf",
+    "url": "//umeshn95.github.io/static/js/2.73e6d291.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "//umeshn95.github.io/static/js/2.73e6d291.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "aeea57ee16586d15847b",
+    "url": "//umeshn95.github.io/static/js/main.603ff859.chunk.js"
+  },
+  {
+    "revision": "0b0a4114ddea23dbe480",
+    "url": "//umeshn95.github.io/static/js/runtime-main.323c50c4.js"
+  },
+  {
+    "revision": "9d8b34cd161015e797f978bd04c28fa3",
+    "url": "//umeshn95.github.io/static/media/polygon.9d8b34cd.svg"
+  },
+  {
+    "revision": "7ea0428df82050816f91151b79c50a59",
+    "url": "//umeshn95.github.io/static/media/svg-1.7ea0428d.svg"
+  },
+  {
+    "revision": "53709f373b6ad4275242f0eda9411164",
+    "url": "//umeshn95.github.io/static/media/svg-10.53709f37.svg"
+  },
+  {
+    "revision": "adc34674fc3ef939ec64e504fc22e7a0",
+    "url": "//umeshn95.github.io/static/media/svg-11.adc34674.svg"
+  },
+  {
+    "revision": "aa22dc2a164247c9bc7502d1c1cd49d4",
+    "url": "//umeshn95.github.io/static/media/svg-12.aa22dc2a.svg"
+  },
+  {
+    "revision": "f520744aba1da5630be11c6e108e6c05",
+    "url": "//umeshn95.github.io/static/media/svg-13.f520744a.svg"
+  },
+  {
+    "revision": "650f0fc793776bbf2765c776be5c8ff6",
+    "url": "//umeshn95.github.io/static/media/svg-14.650f0fc7.svg"
+  },
+  {
+    "revision": "47298adf887e493776c4580adf46a037",
+    "url": "//umeshn95.github.io/static/media/svg-15.47298adf.svg"
+  },
+  {
+    "revision": "2412e7d756317d91a4c4297dc92735fc",
+    "url": "//umeshn95.github.io/static/media/svg-16.2412e7d7.svg"
+  },
+  {
+    "revision": "e4d107c0b94303d5d90280d4109f0c93",
+    "url": "//umeshn95.github.io/static/media/svg-17.e4d107c0.svg"
+  },
+  {
+    "revision": "63f7ad89613ddc507e7b49096f964c87",
+    "url": "//umeshn95.github.io/static/media/svg-18.63f7ad89.svg"
+  },
+  {
+    "revision": "d4846e1163a89864dd49fb03ec6308ed",
+    "url": "//umeshn95.github.io/static/media/svg-2.d4846e11.svg"
+  },
+  {
+    "revision": "35c54de9865aa5255e13be4c2cd8c038",
+    "url": "//umeshn95.github.io/static/media/svg-5.35c54de9.svg"
+  },
+  {
+    "revision": "8f5dc51b9e1fc35284cf9e39e0b6d4b5",
+    "url": "//umeshn95.github.io/static/media/svg-6.8f5dc51b.svg"
+  },
+  {
+    "revision": "32a59fee9c4c2dc64a75383192ed9b5b",
+    "url": "//umeshn95.github.io/static/media/svg-7.32a59fee.svg"
+  },
+  {
+    "revision": "885dc68bf623fcf56cd981b084d93006",
+    "url": "//umeshn95.github.io/static/media/svg-8.885dc68b.svg"
+  },
+  {
+    "revision": "8c666d4f0c7d2b4e6baa2fd95dfd6c06",
+    "url": "//umeshn95.github.io/static/media/svg-9.8c666d4f.svg"
+  },
+  {
+    "revision": "b9408f8816bef6bd1737545e3a9773af",
+    "url": "//umeshn95.github.io/static/media/triangle.b9408f88.svg"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/precache-manifest.207d86ca629380c55d8b3e84b3df8995.js"
+  "//umeshn95.github.io/precache-manifest.ac2ef34fe4f367e25a1bc1dbfd25def4.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("//umeshn95.github.io/index.html"), {
   
   blacklist: [/^\/_/,/\/[^/?]+\.[^/]+$/],
 });
